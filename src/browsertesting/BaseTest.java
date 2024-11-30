0ae2f650205e60daf6e649bb1c485c8fc4bbc00a
prmc924 <prcm924@gmail.com>
2024-11-30 17:21:08 +0000

    Today's class update

--- src/browsertesting/BaseTest.java
+++ src/browsertesting/BaseTest.java
@@ -3,8 +3,6 @@ package browsertesting;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import java.time.Duration;
-
 /**
  * Created by Jay Vaghani
  */
@@ -16,7 +14,7 @@ public class BaseTest {
         driver = new ChromeDriver();
         driver.get(baseUrl);
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+//        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
     }
 
     public void closeBrowser(){
